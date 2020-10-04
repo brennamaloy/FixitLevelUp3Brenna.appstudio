e1a84dfc27b8a525af8615fa497560bea39dfe4a
brennamaloy <bkm91466@creighton.edu>
2020-10-04 17:06:55 -0500

    Update HousePayment.js

--- forms/HousePayment/HousePayment.js
+++ forms/HousePayment/HousePayment.js
@@ -1,8 +1,28 @@
-function houseLoanPayment (p, r, n) {
-    return (p * (r*((1+r)**n))/(((1+r)**n)-1))
+function houseLoanPayment15(p,r) {
+    return (p * (r*((1+r)**180))/(((1+r)**180)-1))
+}
+
+function houseLoanPayment30(p,r) {
+    return (p * (r*((1+r)**360))/(((1+r)**360)-1))
+}
+
+btn15years.onclick=function calcHouseLoanPayment15(){
+  let principleValue = inptPrinciple1.value
+  let monthlyInterest = inptMonthlyInterest1.value
+  let housePayment15 =  houseLoanPayment15(Number(principleValue),Number(monthlyInterest))
+  lblAnswer1.hidden = false
+  lblAnswer1.textContent = ("A home loan for $ " + principleValue + " over 15 years at " + monthlyInterest +
+  " % would have a monthly payment of $ " + housePayment15)
 }
 
 
-btnCalculate1.onclick=function houseLoanPayment(){
-  
+btn30years.onclick=function calcHouseLoanPayment30(){
+  let principleValue = inptPrinciple1.value
+  let monthlyInterest = inptMonthlyInterest1.value
+  let housePayment30 =  houseLoanPayment30(Number(principleValue),Number(monthlyInterest))
+  lblAnswer1.hidden = false
+  lblAnswer1.textContent = ("A home loan for $ " + principleValue + " over 30 years at " + monthlyInterest +
+  " % would have a monthly payment of $ " + housePayment30)
 }
+
+
