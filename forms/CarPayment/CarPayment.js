bc972ff85e8a97159ddbfaabfca52636edd7ce38
brennamaloy <bkm91466@creighton.edu>
2020-10-04 17:13:07 -0500

    Update CarPayment.js

--- forms/CarPayment/CarPayment.js
+++ forms/CarPayment/CarPayment.js
@@ -7,7 +7,7 @@ btnCalculate.onclick=function calcCarLoanPayment() {
   let principleValue = inptPrinciple.value
   let monthlyInterest = inptMonthlyInterest.value
   let totalMonths = inptTotalMonths.value
-  let carPayment =  carLoanPayment(Number(principleValue),Number(monthlyInterest),Number(totalMonths))
+  let carPayment =  carLoanPayment(Number(principleValue),Number(monthlyInterest)/100,Number(totalMonths))
   lblAnswer.hidden = false
   lblAnswer.textContent = ("A car loan for $ " + principleValue + " over " + totalMonths + " months at " + monthlyInterest +
   " % would have a monthly payment of $ " + carPayment)
